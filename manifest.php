4ba40355de74ff352dc6feee31dcb7f8c78a2c4e
Ionut Padurariu <ionut@taotesting.com>
2018-01-22 09:42:35 +0100

    bump version

--- manifest.php
+++ manifest.php
@@ -11,11 +11,11 @@ return array(
 	'name' => 'taoAltResultStorage',
     'label' => 'Result storage key-value implementation',
 	'description' => 'Implements Alternative Result storage results interface using persistencies',
-	'version' => '3.0.1',
+	'version' => '3.1.0',
     'license' => 'GPL-2.0',
 	'author' => 'Open Assessment Technologies',
 	'requires' => [
-        'taoResultServer' => '>=4.2.0'
+        'taoResultServer' => '>=5.2.0'
 	],
 	'models' => [
 		'http://www.tao.lu/Ontologies/taoAltResultStorage.rdf#'
