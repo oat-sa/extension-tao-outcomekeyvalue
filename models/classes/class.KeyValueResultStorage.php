d41fe13a0a2849de3b09c852a3d278e4bd551cc7
Ihar Aleksiaichuk <ihar.aleksiaichuk@1pt.com>
2019-03-01 12:56:25 +0300

    new period options

--- models/classes/class.KeyValueResultStorage.php
+++ models/classes/class.KeyValueResultStorage.php
@@ -62,6 +62,15 @@ class taoAltResultStorage_models_classes_KeyValueResultStorage extends Configura
      */
     const PROPERTY_SEPARATOR = '_prop_';
 
+
+    /**
+     *  period FILTER variables
+     */
+    const PARAM_START_FROM = 'startfrom';
+    const PARAM_START_TO = 'startto';
+    const PARAM_END_FROM = 'endfrom';
+    const PARAM_END_TO = 'endto';
+
     /**
      * @var common_persistence_AdvKeyValuePersistence
      */
@@ -451,26 +460,26 @@ class taoAltResultStorage_models_classes_KeyValueResultStorage extends Configura
 
             $execution = ServiceProxy::singleton()->getDeliveryExecution($key);
 
-            if (array_key_exists('startfrom', $options) && $options['startfrom'] !== false) {
-                if (\tao_helpers_Date::getTimeStamp($execution->getStartTime(), false) <= $options['startfrom']) {
+            if (array_key_exists(self::PARAM_START_FROM, $options) && $options[self::PARAM_START_FROM] !== false) {
+                if (\tao_helpers_Date::getTimeStamp($execution->getStartTime(), false) <= $options[self::PARAM_START_FROM]) {
                     continue;
                 }
 
             }
-            if (array_key_exists('startto', $options) && $options['startto'] !== false) {
-                if (\tao_helpers_Date::getTimeStamp($execution->getStartTime(), false) >= $options['startto']) {
+            if (array_key_exists(self::PARAM_START_TO, $options) && $options[self::PARAM_START_TO] !== false) {
+                if (\tao_helpers_Date::getTimeStamp($execution->getStartTime(), false) >= $options[self::PARAM_START_TO]) {
                     continue;
                 }
 
             }
-            if (array_key_exists('endfrom', $options) && $options['endfrom'] !== false) {
-                if (\tao_helpers_Date::getTimeStamp($execution->getFinishTime(), false) <= $options['endfrom']) {
+            if (array_key_exists(self::PARAM_END_FROM, $options) && $options[self::PARAM_END_FROM] !== false) {
+                if (\tao_helpers_Date::getTimeStamp($execution->getFinishTime(), false) <= $options[self::PARAM_END_FROM]) {
                     continue;
                 }
 
             }
-            if (array_key_exists('endto', $options) && $options['endto'] !== false) {
-                if (\tao_helpers_Date::getTimeStamp($execution->getFinishTime(), false) >= $options['endto']) {
+            if (array_key_exists(self::PARAM_END_TO, $options) && $options[self::PARAM_END_TO] !== false) {
+                if (\tao_helpers_Date::getTimeStamp($execution->getFinishTime(), false) >= $options[self::PARAM_END_TO]) {
                     continue;
                 }
 
